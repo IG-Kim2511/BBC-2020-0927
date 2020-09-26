a772f80b0bed84a85ad7f4dbde0edbbdd3913446
IG-Kim <iikim2511@gmail.com>
2020-09-26 16:45:35 -0400

    get ready

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+// ●
+/* part3 */
+document.write("part3-2 event");
+console.log(" part3-2 event");
+
+//  C 58 . JS 58
+
+// [Selectors]
+
+// [Event Listeners]
+
+// [Funcitons]
+function name(event) {
+  // todo DIV
+  // Create LI
+}
